f21e7fe44fc6caaac4c96b1537cf129b8b492e63
钟媛媛 <zhongyuanyuan@zhongyuanyuandeMacBook-Pro.local>
2021-06-15 22:23:51 +0800

    fix:build问题

--- build/demo/page/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { FC } from 'react';
-import './index.less';
-interface testProps {
-}
-declare const Demo: FC<testProps>;
-export default Demo;
--- build/src/reactPullRefresh.d.ts
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react';
-import './index.less';
-import { homeProps } from './propType';
-declare const ReactPullRefresh: FC<homeProps>;
-export default ReactPullRefresh;
